e898c8588c5e532b948b5298ee7279eb69825169
Mauricio Alvarez <maurialvarez103@gmail.com>
2022-07-18 21:25:37 -0300

    modifico cosas en banner

--- Agregar.php
+++ Agregar.php
@@ -8,7 +8,7 @@ $tipoImagen = $_FILES['imagen']['type'];
 $tamanioImagen = $_FILES['imagen']['size'];
 $url = $_POST['url'];
 
-if ($tamanioImagen <= 1500000) {
+if ($tamanioImagen <= 15000000) {
     if ($tipoImagen == "image/jpeg" || $tipoImagen == "image/jpg" || $tipoImagen == "image/png") {
         $carpetaDestino = $_SERVER['DOCUMENT_ROOT'] . "/BackOffice/imagenes/";
         move_uploaded_file($_FILES['imagen']['tmp_name'], $carpetaDestino.$nombreImagen);
@@ -16,7 +16,7 @@ if ($tamanioImagen <= 1500000) {
         echo "solo imagenes con extencion jpg, jpeg, png...";
     }
 }else{
-    echo "Maximo 1,5 MB";
+    echo "Maximo 15 MB";
 }
 
 
--- config.php
+++ config.php
@@ -5,6 +5,6 @@
     const IP_DB = "127.0.0.1";
     const USER_DB = "root";
     const PASS_DB = "";
-    const NAME_DB = "PruebaBD";
+    const NAME_DB = "Prueba";
     const PORT_DB = "3306";
    
